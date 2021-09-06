a5bc693f158b0c4b0420e8c17c4217cb2776c063
madusankanipunajith <madushankanipunajith@gmail.com>
2021-09-06 12:44:23 +0530

    heroku updates

--- backend/app.js
+++ backend/app.js
@@ -65,11 +65,23 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}else{
+  app.get("/",(req,res)=>{
+    res.send("Api running");
+  })
+}
+
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'production' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
